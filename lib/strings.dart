0d65b3d6873433acd1c3b660a55dedf16509f1c5
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-11-07 11:07:51 +0200

    trimming strings

--- lib/strings.dart
+++ lib/strings.dart
@@ -3,4 +3,9 @@ void runningStrings() {
   const userInput = 'sPoNgEbOb@eXaMpLe.cOm';
   final lowercase = userInput.toLowerCase();
   print('original input: $userInput, lowercase output: $lowercase');
+
+  // trimming strings
+  const usrInput = ' 221B Baker St. ';
+  final trimmed = usrInput.trim();
+  print('original input: $usrInput, trimmed output: $trimmed'); // '221B Baker St.'
 }
