1c70f7c26c4623b7db577fef2dc98a14defc9e20
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-11-07 11:19:03 +0200

    splitting strings into a list

--- lib/strings.dart
+++ lib/strings.dart
@@ -18,7 +18,15 @@ void runningStrings() {
     2,
     '0',
   ); // get remaining minutes by modulo
-  final seconds = '${time.inSeconds % 60}'.padLeft(2, '0'); // get remaining seconds by modulo
+  final seconds = '${time.inSeconds % 60}'.padLeft(
+    2,
+    '0',
+  ); // get remaining seconds by modulo
   final timeString = '$hours:$minutes:$seconds';
   print(timeString); // 1:02:07
+
+  // splitting strings into a List
+  const csvFileLine = 'Martin,Emma,12,Paris,France';
+  final fields = csvFileLine.split(',');
+  print('split list: $fields');
 }
