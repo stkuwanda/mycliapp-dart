6568d436b57697a51a820310bf66ee15302da95f
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-11-07 11:16:36 +0200

    padding strings

--- lib/strings.dart
+++ lib/strings.dart
@@ -7,5 +7,18 @@ void runningStrings() {
   // trimming strings
   const usrInput = ' 221B Baker St. ';
   final trimmed = usrInput.trim();
-  print('original input: $usrInput, trimmed output: $trimmed'); // '221B Baker St.'
+  print(
+    'original input: $usrInput, trimmed output: $trimmed',
+  ); // '221B Baker St.'
+
+  // padding strings
+  final time = Duration(hours: 1, minutes: 2, seconds: 7);
+  final hours = time.inHours;
+  final minutes = '${time.inMinutes % 60}'.padLeft(
+    2,
+    '0',
+  ); // get remaining minutes by modulo
+  final seconds = '${time.inSeconds % 60}'.padLeft(2, '0'); // get remaining seconds by modulo
+  final timeString = '$hours:$minutes:$seconds';
+  print(timeString); // 1:02:07
 }
