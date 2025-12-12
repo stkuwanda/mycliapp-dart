8a2e5de9fb6a488b55c9ab420f1bba6aa46b22a0
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-12-12 11:00:44 +0200

    added comments on substrings

--- bin/substring.dart
+++ bin/substring.dart
@@ -3,22 +3,25 @@ void extractFromTextWithSubstring() {
 <h1>Dart Tutorial</h1>
 <h1>Flutter Tutorial</h1>
 <h1>Other Tutorials</h1>
-''';
+'''; // multiline string
 
-  var position = 0;
+  var position = 0; // initial position
 
+  // loop to find all headings
   while (true) {
-    var start = text.indexOf('<h1>', position) + '<h1>'.length;
-    var end = text.indexOf('</h1>', position);
-    print('start index: $start, end index: $end');
+    var start =
+        text.indexOf('<h1>', position) + '<h1>'.length; // find start index
+    var end = text.indexOf('</h1>', position); // find end index
+    print('start index: $start, end index: $end'); // debug print
 
+    // break if no more headings found
     if (end == -1) {
       break;
     }
 
-    final heading = text.substring(start, end);
-    print(heading);
-    position = end + '</h1>'.length;
+    final heading = text.substring(start, end); // extract heading
+    print(heading); // print heading
+    position = end + '</h1>'.length; // update position
   }
 }
 
@@ -27,13 +30,13 @@ void extractFromTextWithRegExp() {
   <h1>Dart Tutorial</h1>
   <h1>Flutter Tutorial</h1>
   <h1>Other Tutorials</h1>
-  ''';
+  '''; // multiline string
 
-  final headings = RegExp(r'<h1>(.+)</h1>');
-  final matches = headings.allMatches(text);
+  final headings = RegExp(r'<h1>(.+)</h1>'); // regular expression
+  final matches = headings.allMatches(text); // all matches
 
   for (final match in matches) {
-    print(match.group(1));
+    print(match.group(1)); // print the first capturing group
   }
 }
 
