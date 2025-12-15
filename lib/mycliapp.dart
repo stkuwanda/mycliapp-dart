f41d36e3c06bb8fc39ce3dcc7baaaa07a5bdad68
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-12-15 14:26:54 +0200

    top level functions

--- lib/mycliapp.dart
+++ lib/mycliapp.dart
@@ -1,3 +1,13 @@
+// top level function
 int calculate() {
+  // a function declared inside another function
+  int foo() {
+    print('hey');
+
+    return 1;
+  }
+
+  print(foo());
+
   return 6 * 7;
 }
