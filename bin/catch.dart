013b0febb092e44d62c9cb3742c9a603995bb55b
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-12-20 17:08:54 +0200

    throw custom exceptions

--- bin/catch.dart
+++ bin/catch.dart
@@ -1,5 +1,92 @@
 import 'dart:convert';
 
+// Custom exception classes
+class ShortPasswordException implements Exception {
+  final String message;
+
+  ShortPasswordException(this.message);
+}
+
+class NoNumberException implements Exception {
+  final String message;
+
+  NoNumberException(this.message);
+}
+
+class NoUppercaseException implements Exception {
+  final String message;
+
+  NoUppercaseException(this.message);
+}
+
+class NoLowercaseException implements Exception {
+  final String message;
+
+  NoLowercaseException(this.message);
+}
+
+class NoSpecialCharacterException implements Exception {
+  final String message;
+
+  NoSpecialCharacterException([
+    this.message = 'Password must have a special character!',
+  ]);
+}
+
+// Validate that the password has at least 12 characters
+void validateLength(String password) {
+  final goodLength = RegExp(r'.{12,}');
+
+  if (!password.contains(goodLength)) {
+    throw ShortPasswordException('Password must be at least 12 characters!');
+  }
+}
+
+// Validate that the password has at least one lowercase letter
+void validateLowercase(String password) {
+  final lowercase = RegExp(r'[a-z]');
+
+  if (!password.contains(lowercase)) {
+    throw NoLowercaseException('Password must have a lowercase letter!');
+  }
+}
+
+// Validate that the password has at least one uppercase letter
+void validateUppercase(String password) {
+  final uppercase = RegExp(r'[A-Z]');
+
+  if (!password.contains(uppercase)) {
+    throw NoUppercaseException('Password must have an uppercase letter!');
+  }
+}
+
+// Validate that the password has at least one number
+void validateNumber(String password) {
+  final number = RegExp(r'[0-9]');
+
+  if (!password.contains(number)) {
+    throw NoNumberException('Password must have a number!');
+  }
+}
+
+// Validate that the password has at least one special character
+void validateSpecialCharacters(String password) {
+  final specialChar = RegExp(r'[^\w\s]'); // Special characters
+
+  if (!password.contains(specialChar)) {
+    throw NoSpecialCharacterException();
+  }
+}
+
+// Validate the password by calling all validation functions
+void validatePassword(String password) {
+  validateLength(password);
+  validateLowercase(password);
+  validateUppercase(password);
+  validateNumber(password);
+  validateSpecialCharacters(password);
+}
+
 void run() {
   const json = '{"name": "simba"}';
 
@@ -46,4 +133,5 @@ void runCatchMultipleExceptions() {
 void main() {
   // run();4
   runCatchMultipleExceptions();
+  validatePassword('hbdbTiiiiih123ui !');
 }
