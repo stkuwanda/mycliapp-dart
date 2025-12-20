b2627495bfead5178bcc5bfa95afaed24cb63504
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-12-20 13:54:46 +0200

    catch multiple specified exceptions

--- bin/catch.dart
+++ bin/catch.dart
@@ -14,12 +14,36 @@ void run() {
   }
 }
 
-// Never type indicates that a function does not return a value 
+// Never type indicates that a function does not return a value
 // and never completes normally.
 Never runThrow() {
   throw 'CustomException: This is an error'; // Throwing an error
 }
 
+void runCatchMultipleExceptions() {
+  void handleFormatException() {
+    print("FormatException: You tried to parse a non-numeric string.");
+  }
+
+  void handleDivisionByZero() {
+    print("UnsupportedError: You can't divide by zero.");
+  }
+
+  const numberStrings = ["42", "hello"];
+
+  try {
+    for (final numberString in numberStrings) {
+      final number = int.parse(numberString);
+      print(number ~/ 0);
+    }
+  } on FormatException {
+    handleFormatException();
+  } on UnsupportedError {
+    handleDivisionByZero();
+  }
+}
+
 void main() {
-  run();
+  // run();4
+  runCatchMultipleExceptions();
 }
