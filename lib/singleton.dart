cafe54170961a3b3bfd1ce0f32b52fd9361fdd32
Simbarashe Kuwanda <stkuwanda@gmail.com>
2026-01-04 16:07:05 +0200

    singleton update

--- lib/singleton.dart
+++ lib/singleton.dart
@@ -1,13 +1,22 @@
 // singleton pattern with private constructor and factory constructor and static instance
 // and unnamed private constructor which ensures only one instance is created
 class Singleton {
-  String name = 'SingletonInstance';
-  String type = 'OnlyOne';
-  static final Singleton _instance = Singleton._();
-
-  // Private construcctor
+  // Private named generative constructor with initializer list
   Singleton._() : name = 'SingletonNameInstance';
 
-  // Factory constructor to return the same instance every time
+  // Factory unnamed constructor to return the same instance every time
   factory Singleton() => _instance;
+
+  // static/class variables first
+  static final Singleton _instance =
+      Singleton._(); // static private variable to hold an instance
+
+  // instance variables 
+  String name = 'SingletonInstance';
+  String type = 'OnlyOne';
+}
+
+void main() {
+  var ent = Singleton();
+  print(ent.name);
 }
